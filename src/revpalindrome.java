467d02093f954f6c01fce2eae85c485184cbc726
Suvarna Pn <suvarnapn86@gmail.com>
2026-02-13 14:51:59 +0530

    revpalindrome

--- src/revpalindrome.java
+++ src/revpalindrome.java
@@ -6,7 +6,7 @@ public class revpalindrome {
             rev += str.charAt(i);
         }
         if (str.equals(rev)) {
-            System.out.println("it is a plainde:)");
+            System.out.println("it is a plaind............e:)");
         } else {
             System.out.println("it is not a palindrome");
         }
