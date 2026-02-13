9f542e0c8f60a1514f2960b1c12fe850e7ddca57
Suvarna Pn <suvarnapn86@gmail.com>
2026-02-13 14:36:15 +0530

    java1.java

---
+++ src/java1.java
@@ -0,0 +1,15 @@
+class java1{
+    public static void main(String[] args) {
+    // Declare two integer variables
+    int number1 = 10;
+    int number2 = 5;
+
+    // Calculate the sum
+    int sum = number1 + number2;
+
+    // Print the result to the console
+    System.out.println("The first number is: " + number1);
+    System.out.println("The second number is: " + number2);
+    System.out.println("The sum of the two numbers is: " + sum);
+}
+}
