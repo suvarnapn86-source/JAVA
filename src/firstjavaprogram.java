95c626fba75668ee727f240807776d932d880909
Suvarna Pn <suvarnapn86@gmail.com>
2026-02-21 22:07:59 +0530

    firstjavaprogram.java

--- src/firstjavaprogram.java
+++ src/firstjavaprogram.java
@@ -1,5 +1,6 @@
 class firstjavaprogram {
     public static void main (String[] args){
         System.out.println("hello hai,welcome to java");
+        System.out.println("hello hai "+"suvarna");
     }
 }
