ce53d59896cece94c16041ee4b1667c2264c246c
Suvarna Pn <suvarnapn86@gmail.com>
2026-02-21 15:05:50 +0530

    a3DArray.java

---
+++ src/a3DArray.java
@@ -0,0 +1,21 @@
+ class a3DArray {
+    public static void main(String [] args){
+        int my3DArray[][][]=new int[3][4][5];
+        for(int i=0;i<3;i++){
+            for(int j=0;j<4;j++){
+                for(int k=0;k<5;k++){
+                    my3DArray[i][j][k]=i+j+k;
+                }
+            }
+        }
+        for(int i=0;i<3;i++){
+            for(int j=0;j<4;j++){
+                for (int k=0;k<5;k++){
+                    System.out.print(my3DArray[i][j][k] + "");
+                }
+                System.out.println();
+            }
+            System.out.println();
+            }
+    }
+}
