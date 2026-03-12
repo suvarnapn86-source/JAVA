0761afc85f4fe777b88bdf956daafce0dedc4a3f
Suvarna Pn <suvarnapn86@gmail.com>
2026-03-12 12:36:45 +0530

    TestMultipleCatchBlock.java

---
+++ src/TestMultipleCatchBlock.java
@@ -0,0 +1,18 @@
+class TestMultipleCatchBlock {
+    public static void main(String[] args){
+        try {
+            int a[] = new int[5];
+            a[10] = 50;
+        }
+        catch(ArithmeticException e) {
+            System.out.println("task 1is completed");
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("task 2 completed");
+        }
+        catch(Exception e) {
+            System.out.println("common task completed");
+        }
+        System.out.println("rest pof the code...");
+        }
+    }
