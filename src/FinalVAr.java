01b5f532f4f2fa04501730024b9cb75c57f351f1
Suvarna Pn <suvarnapn86@gmail.com>
2026-03-12 12:45:17 +0530

    FinalVar.java

---
+++ src/FinalVAr.java
@@ -0,0 +1,10 @@
+ class FinalVAr {
+     public static void main(String[] args) {
+         final double PI = 3.14;
+         double radius = 5;
+         double area = PI * radius * radius;
+         System.out.println("radius:" + radius);
+         System.out.println("Area of circle:" + area);
+     }
+ }
+
