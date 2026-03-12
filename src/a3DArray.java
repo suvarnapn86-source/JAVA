c84deeb698641a3b295adc5dd7f63da8d18e6182
Suvarna Pn <suvarnapn86@gmail.com>
2026-03-12 10:20:19 +0530

    a3DArray.java

--- src/a3DArray.java
+++ src/a3DArray.java
@@ -5,7 +5,9 @@
             for(int j=0;j<4;j++){
                 for(int k=0;k<5;k++){
                     my3DArray[i][j][k]=i+j+k;
-                }
+                }                                     //this program creates a 3D integer array of size 3*4*5and fils,each
+                //each element with the sum of its indices(i+j+k),it is uses nested loops to print all the array elements ina structred format
+
             }
         }
         for(int i=0;i<3;i++){
