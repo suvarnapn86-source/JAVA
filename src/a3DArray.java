793b4e0b26747044b262f04a9b42fd624cd400a5
Suvarna Pn <suvarnapn86@gmail.com>
2026-03-13 18:45:20 +0530

    added java programs

--- src/a3DArray.java
+++ src/a3DArray.java
@@ -1,6 +1,6 @@
  class a3DArray {
     public static void main(String [] args){
-        int my3DArray[][][]=new int[3][4][5];
+        int my3DArray[][][]=new int[3][4][6];
         for(int i=0;i<3;i++){
             for(int j=0;j<4;j++){
                 for(int k=0;k<5;k++){
